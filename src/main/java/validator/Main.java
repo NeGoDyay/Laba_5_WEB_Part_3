09cd0f474097a2074fd88289de30a91cddbe5045
NikitaYasiulevich <yasiulevichnikita@gmail.com>
2023-11-12 19:15:12 +0300

    All code added

---
+++ src/main/java/validator/Main.java
@@ -0,0 +1,11 @@
+package validator;
+
+import menu.Menu;
+
+public class Main {
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.IntroducingMenu();
+    }
+}
+
